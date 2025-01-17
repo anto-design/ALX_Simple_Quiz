c03d79167ffe66d43f05364a95051ab1d6b7e7af
anto-design <spartz6122@gmail.com>
2025-01-17 15:29:03 +0300

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -2,15 +2,16 @@ function checkAnswer() {
   const correctAnswer = "4";
 
 
-  if(userAnswer === correctAnswer) {
+  
+}
+
+const userAnswer = document.querySelector('input[name="quiz"]:checked');
+console.log(userAnswer)
+
+
+if(userAnswer === correctAnswer) {
   document.querySelector('feedback').textContent = 'Correct! Well done.'
   }else{
   document.querySelector('feedback').textContent = "That's incorrect. Try again!."
 }
-}
-
-const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-
-
-
   
